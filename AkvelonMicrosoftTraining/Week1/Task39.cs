a8b565196866eb01bda78ea7e4471f7bfef4c9cc
George Pavlovych <pavlheo@hotmail.com>
2016-04-08 22:17:03 +0300

    Task 39 of Week 1 is solved

---
+++ AkvelonMicrosoftTraining/Week1/Task39.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Week1
+{
+    public static class Task39
+    {
+        public struct Duplicate
+        {
+            public int Value;
+
+            public int Index;
+        }
+
+        public static IEnumerable<Duplicate> Solve(int[] input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            var result = new List<Duplicate>();
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input[Math.Abs(input[i])] > 0)
+                {
+                    input[Math.Abs(input[i])] = -input[Math.Abs(input[i])];
+                }
+                else
+                {
+                    result.Add(new Duplicate() { Index = i, Value = Math.Abs(input[i]) });
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Week1Tests/Task39Tests.cs
@@ -0,0 +1,30 @@
+﻿namespace Week1.Tests
+{
+    using System.Collections.Generic;
+
+    using FluentAssertions;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Task39Tests
+    {
+        [TestMethod]
+        public void TestSolve()
+        {
+            //arrange
+            var input = new[] { 1, 1, 2, 3, 2 };
+            var expectedOutput = new[]
+                                     {
+                                         new Task39.Duplicate() { Index = 1, Value = 1 },
+                                         new Task39.Duplicate() { Index = 4, Value = 2 }
+                                     };
+
+            //act
+            var result = Task39.Solve(input);
+
+            //assert
+            result.Should().Contain(expectedOutput);
+        }
+    }
+}
