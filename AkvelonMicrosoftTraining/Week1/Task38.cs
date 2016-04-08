a78ab367e0bd21d74793ecfa851363bf22032b0c
George Pavlovych <pavlheo@hotmail.com>
2016-04-08 21:21:08 +0300

    Task 38 of Week1 solved

---
+++ AkvelonMicrosoftTraining/Week1/Task38.cs
@@ -0,0 +1,53 @@
+﻿namespace Week1
+{
+    using System;
+
+    /// <summary>
+    /// Maximum subarray problem implementation: solve with O(N) complexity
+    /// </summary>
+    public static class Task38
+    {
+        public static double[] FindMaximumSubarray(double[] input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            if (input.Length == 0)
+            {
+                return new double[] {};
+            }
+            var maxEndingHere = 0.0;
+            var maxSoFar = 0.0;
+            var startIndex = 0;
+            var maxSoFarIndex = 0;
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (maxEndingHere == 0.0)
+                {
+                    startIndex = i;
+                }
+                var newValue = maxEndingHere + input[i];
+                if (newValue > 0)
+                {
+                    maxEndingHere = newValue;
+                }
+                else
+                {
+                    maxEndingHere = 0;
+                }
+                if (maxSoFar < maxEndingHere)
+                {
+                    maxSoFarIndex = i;
+                    maxSoFar = maxEndingHere;
+                }
+            }
+            var result = new double[maxSoFarIndex - startIndex + 1];
+            for (var i = startIndex; i <= maxSoFarIndex; i++)
+            {
+                result[i - startIndex] = input[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Week1Tests/Task38Tests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace Week1.Tests
+{
+    using System;
+
+    [TestClass]
+    public class Task38Tests
+    {
+        [TestMethod]
+        public void FindMaximumSubarrayTest()
+        {
+            //arrange
+            var input = new double[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            var expectedOutput = new double[] { 4, -1, 2, 1 };
+
+            //act
+            var actualOutput = Task38.FindMaximumSubarray(input);
+
+            //assert
+            actualOutput.ShouldBeEquivalentTo(expectedOutput);
+        }
+
+        [TestMethod]
+        public void FindMaximumSubarrayTestEmptyValue()
+        {
+            //arrange
+            var input = new double[] { };
+            var expectedOutput = new double[] { };
+
+            //act
+            var actualOutput = Task38.FindMaximumSubarray(input);
+
+            //assert
+            actualOutput.ShouldBeEquivalentTo(expectedOutput);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FindMaximumSubarrayTestNullValue()
+        {
+            //act
+            Task38.FindMaximumSubarray(null);
+        }
+    }
+}
