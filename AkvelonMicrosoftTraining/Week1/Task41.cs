f39826a1e112d07989338c2a85c11c2bc5f8f10c
George Pavlovych <pavlheo@hotmail.com>
2016-04-10 00:26:30 +0300

    Task 41 of week 1 solved

---
+++ AkvelonMicrosoftTraining/Week1/Task41.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1
+{
+    /// <summary>
+    /// Convert unsinged long to string, given putchar only
+    /// </summary>
+    public static class Task41
+    {
+        public static string PutLong(ulong number)
+        {
+            if (number == 0)
+            {
+                return "0";
+            }
+            var result = new StringBuilder();
+            var stack = new Stack<char>();
+            while (number != 0)
+            {
+                stack.Push((char)(number % 10 + (byte)'0'));
+                number /= 10;
+            }
+            while (stack.Count > 0)
+            {
+                result.Append(stack.Pop());
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Week1Tests/Task41Tests.cs
@@ -0,0 +1,51 @@
+﻿namespace Week1Tests
+{
+    using BaseTests.Tests;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Week1;
+
+    [TestClass]
+    public class Task41Tests : UnitTestBase
+    {
+        private void PutLongTest(ulong number)
+        {
+            //act
+            var actual = Task41.PutLong(number);
+
+            //assert
+            Assert.AreEqual(number.ToString(), actual);
+        }
+
+        [TestMethod]
+        public void PutLongTest1054352312400()
+        {
+            PutLongTest(1054352312400);
+        }
+
+        [TestMethod]
+        public void PutLongTest100()
+        {
+            PutLongTest(100);
+        }
+
+        [TestMethod]
+        public void PutLongTest10()
+        {
+            PutLongTest(10);
+        }
+
+        [TestMethod]
+        public void PutLongTest1()
+        {
+            PutLongTest(1);
+        }
+
+        [TestMethod]
+        public void PutLongTest0()
+        {
+            PutLongTest(0);
+        }
+    }
+}
