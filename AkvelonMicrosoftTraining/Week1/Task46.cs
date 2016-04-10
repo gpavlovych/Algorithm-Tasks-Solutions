6e40f09524f284edae416a8ea959b67c4b66f2b5
George Pavlovych <pavlheo@hotmail.com>
2016-04-10 04:00:39 +0300

    Task 46 of Week1 solved

---
+++ AkvelonMicrosoftTraining/Week1/Task46.cs
@@ -0,0 +1,13 @@
+﻿namespace Week1
+{
+    /// <summary>
+    /// Fast way to multiply by 7
+    /// </summary>
+    public static class Task46
+    {
+        public static long MultiplyBy7(long number)
+        {
+            return (number << 3) - number;
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Week1Tests/Task46Tests.cs
@@ -0,0 +1,40 @@
+﻿using BaseTests.Tests;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week1.Tests
+{
+    [TestClass]
+    public class Task46Tests : UnitTestBase
+    {
+        private void MultiplyBy7Test(int value)
+        {
+            //arrange
+            var expectedResult = value * 7;
+
+            //act
+            var actualResult = Task46.MultiplyBy7(value);
+
+            //assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void Multiply0By7Test()
+        {
+            MultiplyBy7Test(0);
+        }
+
+        [TestMethod]
+        public void Multiply9By7Test()
+        {
+            MultiplyBy7Test(9);
+        }
+
+        [TestMethod]
+        public void MultiplyMinus19By7Test()
+        {
+            MultiplyBy7Test(-19);
+        }
+    }
+}
