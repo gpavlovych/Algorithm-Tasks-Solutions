fd597df3c74e93dd021670930f4e8d5ba7ebad5e
George Pavlovych <pavlheo@hotmail.com>
2016-04-10 03:42:36 +0300

    Task45 of week1 solved

---
+++ AkvelonMicrosoftTraining/Week1/Task45.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Week1
+{
+    /// <summary>
+    /// Add to the sorted list
+    /// </summary>
+    public static class Task45
+    {
+        public static void AddSortedList<T>(IList<T> list, T item) where T : IComparable<T>
+        {
+            if (list == null)
+            {
+                throw new ArgumentNullException("list");
+            }
+            list.Add(default(T));
+            for (var index = list.Count - 1; index > 0; index--)
+            {
+                if (list[index - 1].CompareTo(item) > 0)
+                {
+                    list[index] = list[index - 1];
+                }
+                else
+                {
+                    list[index] = item;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Week1Tests/Task45Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+using BaseTests.Tests;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week1.Tests
+{
+    [TestClass]
+    public class Task45Tests: UnitTestBase
+    {
+        [TestMethod]
+        public void AddSortedListTest()
+        {
+            //arrange
+            var item = 5;
+            var initialList = new List<int>() { 1, 2, 3, 4, 6, 7, 8 };
+            var expected = new[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            //act
+            Task45.AddSortedList(initialList, item);
+
+            //assert
+            AssertCollectionsEqual(expected, initialList);
+        }
+
+        [TestMethod]
+        public void AddSortedListTestEnd()
+        {
+            //arrange
+            var item = 9;
+            var initialList = new List<int>() { 1, 2, 3, 4, 6, 7, 8 };
+            var expected = new[] { 1, 2, 3, 4, 6, 7, 8, 9 };
+
+            //act
+            Task45.AddSortedList(initialList, item);
+
+            //assert
+            AssertCollectionsEqual(expected, initialList);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddSortedListTestNull()
+        {
+            Task45.AddSortedList(null, 3);
+        }
+    }
+}
