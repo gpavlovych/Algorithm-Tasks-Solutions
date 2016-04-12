f8584df6df9fa4f4e272c310ef53f0aaf2c4a549
George Pavlovych <pavlheo@hotmail.com>
2016-04-12 07:49:34 +0300

    Task43 of week1 moved to separate project together with unit tests

---
+++ AkvelonMicrosoftTraining/Task43/Solver.cs
@@ -0,0 +1,44 @@
+﻿namespace Task43
+{
+    using System;
+
+    /// <summary>
+    /// Reverse words (not characters) in sentence, given by array of characters
+    /// </summary>
+    public static class Solver
+    {
+        private static void Swap(char[] a, int index1, int index2)
+        {
+            var temp = a[index1];
+            a[index1] = a[index2];
+            a[index2] = temp;
+        }
+
+        private static void ReverseSubarray(char[] a, int startIndex, int length)
+        {
+            for (var index = 0; index < length / 2; index++)
+            {
+                Swap(a, startIndex + index, startIndex + length - index - 1);
+            }
+        }
+
+        public static void ReverseWords(char[] input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            ReverseSubarray(input, 0, input.Length);
+            int wordStart = 0;
+            for (var currentIndex = 0; currentIndex < input.Length; currentIndex++)
+            {
+                if (input[currentIndex] == ' ')
+                {
+                    ReverseSubarray(input, wordStart, currentIndex - wordStart);
+                    wordStart = currentIndex + 1;
+                }
+            }
+            ReverseSubarray(input, wordStart, input.Length - wordStart);
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Task43Tests/SolverTests.cs
@@ -0,0 +1,64 @@
+﻿namespace Task43Tests
+{
+    using System;
+
+    using BaseTests.Tests;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Task43;
+
+    [TestClass]
+    public class SolverTests : UnitTestBase
+    {
+        [TestMethod]
+        public void ReverseWordsTest()
+        {
+            //arrange
+            char[] input = "very good words choice counts a lot".ToCharArray();
+            char[] expectedArray = "lot a counts choice words good very".ToCharArray();
+
+            //act
+            Solver.ReverseWords(input);
+
+            //assert
+            this.AssertCollectionsEqual(expectedArray, input);
+        }
+
+        [TestMethod]
+        public void ReverseWordsTestSingleWord()
+        {
+            //arrange
+            char[] input = "very".ToCharArray();
+            char[] expectedArray = "very".ToCharArray();
+
+            //act
+            Solver.ReverseWords(input);
+
+            //assert
+            this.AssertCollectionsEqual(expectedArray, input);
+        }
+
+        [TestMethod]
+        public void ReverseWordsTestEmpty()
+        {
+            //arrange
+            char[] input = "".ToCharArray();
+            char[] expectedArray = "".ToCharArray();
+
+            //act
+            Solver.ReverseWords(input);
+
+            //assert
+            this.AssertCollectionsEqual(expectedArray, input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ReverseWordsTestNull()
+        {
+            //act
+            Solver.ReverseWords(null);
+        }
+    }
+}
