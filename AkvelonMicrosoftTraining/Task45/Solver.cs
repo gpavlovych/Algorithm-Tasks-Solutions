c79165e4111faf5fd31ab5ef9cc161931fa2674c
George Pavlovych <pavlheo@hotmail.com>
2016-04-12 13:00:05 +0300

    Task 45 of week 1 is moved to separate project with unit tests

---
+++ AkvelonMicrosoftTraining/Task45/Solver.cs
@@ -0,0 +1,31 @@
+﻿namespace Task45
+{
+    using System;
+
+    /// <summary>
+    /// Add to the sorted list
+    /// </summary>
+    public static class Solver
+    {
+        public static Node<T> AddSortedList<T>(Node<T> list, T item) where T : IComparable<T>
+        {
+            var current = list;
+            var prevNode = default(Node<T>);
+            while (current != null && current.Value.CompareTo(item) < 0)
+            {
+                prevNode = current;
+                current = current.Next;
+            }
+            var newNode = new Node<T>() { Next = current, Value = item };
+            if (prevNode != null)
+            {
+                prevNode.Next = newNode;
+            }
+            if (prevNode == null)
+            {
+                return newNode;
+            }
+            return list;
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Task45Tests/SolverTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week1.Tests
+{
+    using System.Runtime.InteropServices;
+
+    using BaseTests.Tests;
+
+    using Task45;
+
+    [TestClass]
+    public class SolverTests : UnitTestBase
+    {
+        [TestMethod]
+        public void AddSortedListTest()
+        {
+            //arrange
+            var item = 5;
+            var initialNode = BuildTestLinkedList(item, 1, item + 3);
+
+            //act
+            var result = Solver.AddSortedList(initialNode, item);
+
+            //assert
+            VerifyLinkedList(result, 1, item + 3);
+        }
+
+        [TestMethod]
+        public void AddSortedListTestToEmpty()
+        {
+            //arrange
+            var item = 5;
+
+            //act
+            var result = Solver.AddSortedList(null, item);
+
+            //assert
+            Assert.AreEqual(item, result.Value);
+            Assert.IsNull(result.Next);
+        }
+
+        [TestMethod]
+        public void AddSortedListTestEnd()
+        {
+            //arrange
+            var item = 9;
+            var initialList = BuildTestLinkedList(item, 1, item - 1);
+
+            //act
+            var result = Solver.AddSortedList(initialList, item);
+
+            //assert
+            VerifyLinkedList(result, 1, item);
+        }
+
+        [TestMethod]
+        public void AddSortedListTestBeginning()
+        {
+            //arrange
+            var item = 0;
+            var initialList = BuildTestLinkedList(item, 1, 8);
+
+            //act
+            var result = Solver.AddSortedList(initialList, item);
+
+            //assert
+            VerifyLinkedList(result, 0, 8);
+        }
+
+        private static Node<int> BuildTestLinkedList(int item, int start, int end)
+        {
+            var initialNode = new Node<int>() { Value = start };
+            var currentNode = initialNode;
+            for (var i = start + 1; i <= end; i++)
+            {
+                if (i != item)
+                {
+                    currentNode.Next = new Node<int>() { Value = i };
+                    currentNode = currentNode.Next;
+                }
+            }
+            return initialNode;
+        }
+
+        private static void VerifyLinkedList(Node<int> result, int start, int end)
+        {
+            for (var i = start; i <= end; i++)
+            {
+                Assert.AreEqual(i, result.Value);
+                result = result.Next;
+            }
+            Assert.IsNull(result);
+        }
+    }
+}
