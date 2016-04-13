a21e2144737e55dce9dcd2e8cf641b1ef48f234f
George Pavlovych <pavlheo@hotmail.com>
2016-04-13 16:38:09 +0300

    Task 51 of Week 1 solved

---
+++ AkvelonMicrosoftTraining/Task51/Solver.cs
@@ -0,0 +1,27 @@
+﻿namespace Task51
+{
+    using System.Collections.Generic;
+    using System.Text;
+
+    public static class Solver
+    {
+        public static string DeleteAndClean(string s1, string s2)
+        {
+            s1 = s1 ?? "";
+            s2 = s2 ?? "";
+            var dict = new Dictionary<char, bool>();
+
+            foreach (var cs1 in s1)
+            {
+                dict[cs1] = true;
+            }
+            var result = new StringBuilder();
+            foreach (var cs2 in s2)
+                if (!dict.ContainsKey(cs2))
+                {
+                    result.Append(cs2);
+                }
+            return result.ToString();
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Task51Tests/SolverTests.cs
@@ -0,0 +1,96 @@
+﻿namespace Task51Tests
+{
+    using BaseTests.Tests;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Task51;
+
+    [TestClass]
+    public class SolverTests : UnitTestBase
+    {
+        [TestMethod]
+        public void DeleteAndCleanTest()
+        {
+            //arrange
+            var s1 = "Hello1";
+            var s2 = "Hello2";
+
+            //act
+            var result = Solver.DeleteAndClean(s1, s2);
+
+            //assert
+            Assert.AreEqual("2", result);
+        }
+
+        [TestMethod]
+        public void DeleteAndCleanTestSameStrings()
+        {
+            //arrange
+            var s1 = "Hello1";
+            var s2 = "Hello1";
+
+            //act
+            var result = Solver.DeleteAndClean(s1, s2);
+
+            //assert
+            Assert.AreEqual("", result);
+        }
+
+        [TestMethod]
+        public void DeleteAndCleanTestEmptyStringS2()
+        {
+            //arrange
+            var s1 = "Hello1";
+            var s2 = "";
+
+            //act
+            var result = Solver.DeleteAndClean(s1, s2);
+
+            //assert
+            Assert.AreEqual("", result);
+        }
+
+        [TestMethod]
+        public void DeleteAndCleanTestEmptyStringS1()
+        {
+            //arrange
+            var s1 = "";
+            var s2 = "Hello1";
+
+            //act
+            var result = Solver.DeleteAndClean(s1, s2);
+
+            //assert
+            Assert.AreEqual("Hello1", result);
+        }
+
+        [TestMethod]
+        public void DeleteAndCleanTestEmptyStringsBoth()
+        {
+            //arrange
+            var s1 = "";
+            var s2 = "";
+
+            //act
+            var result = Solver.DeleteAndClean(s1, s2);
+
+            //assert
+            Assert.AreEqual("", result);
+        }
+
+        [TestMethod]
+        public void DeleteAndCleanTestNullStringsBoth()
+        {
+            //arrange
+            string s1 = null;
+            string s2 = null;
+
+            //act
+            var result = Solver.DeleteAndClean(s1, s2);
+
+            //assert
+            Assert.AreEqual("", result);
+        }
+    }
+}
