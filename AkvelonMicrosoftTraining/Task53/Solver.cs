77cd34e1175f98ce2abc0a9297bb619cbd1f8cc6
George Pavlovych <pavlheo@hotmail.com>
2016-04-14 04:45:53 +0300

    Task 53 of Week 1 solved

---
+++ AkvelonMicrosoftTraining/Task53/Solver.cs
@@ -0,0 +1,59 @@
+﻿namespace Task53
+{
+    using System;
+    using System.IO;
+
+    public static class Solver
+    {
+        public static void PrintInSpiralOrder<T>(TextWriter writer, T[,] input)
+        {
+            if (writer == null)
+            {
+                throw new ArgumentNullException("writer");
+            }
+
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+
+            int columnIndex = 0, rowIndex = 0;
+            int columnCount = input.GetLength(0), rowCount = input.GetLength(1);
+            while (columnIndex < columnCount && rowIndex < rowCount)
+            {
+                for (var i = rowIndex; i < rowCount; ++i)
+                {
+                    writer.Write("{0} ", input[columnIndex, i]);
+                }
+                columnIndex++;
+
+                /* Print the last column from the remaining columns */
+                for (var i = columnIndex; i < columnCount; ++i)
+                {
+                    writer.Write("{0} ", input[i, rowCount - 1]);
+                }
+                rowCount--;
+
+                /* Print the last row from the remaining rows */
+                if (columnIndex < columnCount)
+                {
+                    for (var i = rowCount - 1; i >= rowIndex; --i)
+                    {
+                        writer.Write("{0} ", input[columnCount - 1, i]);
+                    }
+                    columnCount--;
+                }
+
+                /* Print the first column from the remaining columns */
+                if (rowIndex < rowCount)
+                {
+                    for (var i = columnCount - 1; i >= columnIndex; --i)
+                    {
+                        writer.Write("{0} ", input[i, rowIndex]);
+                    }
+                    rowIndex++;
+                }
+            }
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Task53Tests/SolverTests.cs
@@ -0,0 +1,128 @@
+﻿namespace Task53Tests
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    using BaseTests.Tests;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    using Task53;
+
+    [TestClass]
+    public class SolverTests : UnitTestBase
+    {
+        [TestMethod]
+        public void PrintInSpiralOrderTestSquare()
+        {
+            //arrange
+            var textWriterMock = new Mock<TextWriter>();
+            var array = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            StringBuilder result = new StringBuilder();
+            textWriterMock.Setup(it => it.Write(It.IsAny<string>(), It.IsAny<object>()))
+                .Callback<string, object>((s, i) => result.Append(string.Format(s, i)));
+
+            //act
+            Solver.PrintInSpiralOrder(textWriterMock.Object, array);
+
+            //assert
+            Assert.AreEqual("1 2 3 6 9 8 7 4 5 ", result.ToString());
+        }
+
+        [TestMethod]
+        public void PrintInSpiralOrderTestNonSquare()
+        {
+            //arrange
+            var textWriterMock = new Mock<TextWriter>();
+            var array = new int[,] { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 13, 14, 15 } };
+            StringBuilder result = new StringBuilder();
+            textWriterMock.Setup(it => it.Write(It.IsAny<string>(), It.IsAny<object>()))
+                .Callback<string, object>((s, i) => result.Append(string.Format(s, i)));
+
+            //act
+            Solver.PrintInSpiralOrder(textWriterMock.Object, array);
+
+            //assert
+            Assert.AreEqual("1 2 3 4 5 10 15 14 13 12 11 6 7 8 9 ", result.ToString());
+        }
+
+        [TestMethod]
+        public void PrintInSpiralOrderTestSingleRow()
+        {
+            //arrange
+            var textWriterMock = new Mock<TextWriter>();
+            var array = new int[,] { { 1, 2, 3, 4, 5 } };
+            StringBuilder result = new StringBuilder();
+            textWriterMock.Setup(it => it.Write(It.IsAny<string>(), It.IsAny<object>()))
+                .Callback<string, object>((s, i) => result.Append(string.Format(s, i)));
+
+            //act
+            Solver.PrintInSpiralOrder(textWriterMock.Object, array);
+
+            //assert
+            Assert.AreEqual("1 2 3 4 5 ", result.ToString());
+        }
+
+        [TestMethod]
+        public void PrintInSpiralOrderTestSingleColumn()
+        {
+            //arrange
+            var textWriterMock = new Mock<TextWriter>();
+            var array = new int[,] { { 1 }, { 2 }, { 3 } };
+            StringBuilder result = new StringBuilder();
+            textWriterMock.Setup(it => it.Write(It.IsAny<string>(), It.IsAny<object>()))
+                .Callback<string, object>((s, i) => result.Append(string.Format(s, i)));
+
+            //act
+            Solver.PrintInSpiralOrder(textWriterMock.Object, array);
+
+            //assert
+            Assert.AreEqual("1 2 3 ", result.ToString());
+        }
+
+        [TestMethod]
+        public void PrintInSpiralOrderTestEmpty()
+        {
+            //arrange
+            var textWriterMock = new Mock<TextWriter>();
+            var array = new int[,] { { } };
+            StringBuilder result = new StringBuilder();
+            textWriterMock.Setup(it => it.Write(It.IsAny<string>(), It.IsAny<object>()))
+                .Callback<string, object>((s, i) => result.Append(string.Format(s, i)));
+
+            //act
+            Solver.PrintInSpiralOrder(textWriterMock.Object, array);
+
+            //assert
+            Assert.AreEqual("", result.ToString());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PrintInSpiralOrderTestTextOutNull()
+        {
+            //arrange
+            var array = new int[,] { { } };
+
+            //act
+            Solver.PrintInSpiralOrder(null, array);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PrintInSpiralOrderTestInputNull()
+        {
+            //arrange
+            var textWriterMock = new Mock<TextWriter>();
+            StringBuilder result = new StringBuilder();
+            textWriterMock.Setup(it => it.Write(It.IsAny<string>(), It.IsAny<object>()))
+                .Callback<string, object>((s, i) => result.Append(string.Format(s, i)));
+
+            //act
+            Solver.PrintInSpiralOrder<int>(textWriterMock.Object, null);
+        }
+    }
+}
