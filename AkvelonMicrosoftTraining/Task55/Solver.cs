b2a32acbcb681d567e94a71562dc859a01364247
George Pavlovych <pavlheo@hotmail.com>
2016-04-14 06:02:34 +0300

    Task 55 of week1 fixed

---
+++ AkvelonMicrosoftTraining/Task55/Solver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task55
+{
+    public static class Solver
+    {
+        private static int ClearBit(int input, byte byteNumber)
+        {
+            return input & ~(1 << byteNumber);
+        }
+        public static string ToUpper(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException();
+            }
+            var resultBuilder = new StringBuilder();
+            var inputArray = input.ToCharArray();
+            foreach (var inputItem in inputArray)
+            {
+                var outputItem = inputItem;
+                if ((int)outputItem < 128)
+                {
+                    outputItem = (char)ClearBit(outputItem, 5);
+                }
+                resultBuilder.Append(outputItem);
+            }
+            return resultBuilder.ToString();
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Task55Tests/SolverTests.cs
@@ -0,0 +1,45 @@
+﻿namespace Task55Tests
+{
+    using System;
+
+    using BaseTests.Tests;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Task55;
+
+    [TestClass]
+    public class SolverTests: UnitTestBase
+    {
+        private void ToUpperTest(string input)
+        {
+            //arrange
+            var expectedResult = input.ToUpper();
+
+            //act
+            var result = Solver.ToUpper(input);
+
+            //assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void ToUpperTestNonEmpty()
+        {
+            this.ToUpperTest("SomeTest");
+        }
+
+        [TestMethod]
+        public void ToUpperTestEmpty()
+        {
+            this.ToUpperTest("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ToUpperTestNull()
+        {
+            Solver.ToUpper(null);
+        }
+    }
+}
