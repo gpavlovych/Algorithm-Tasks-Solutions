3e783e8f47846e9ee04be0184a95dd462e6a5fa9
George Pavlovych <pavlheo@hotmail.com>
2016-04-17 00:47:40 +0300

    Fixed https://github.com/gpavlovych/Training/issues/11 Refactored to make use of simple and straightforward recursive algorithm

--- AkvelonMicrosoftTraining/Task50/Solver.cs
+++ AkvelonMicrosoftTraining/Task50/Solver.cs
@@ -1,6 +1,6 @@
 ﻿namespace Task50
 {
-    using System.Collections.Generic;
+    using System;
 
     /// <summary>
     /// Write a function to find the depth of a binary tree.
@@ -13,36 +13,9 @@
             {
                 return 0;
             }
-            int maxDepth = 0;
-            Stack<BinaryTreeNode> stack = new Stack<BinaryTreeNode>();
-            BinaryTreeNode previous = null;
-            stack.Push(topNode);
-            while (stack.Count > 0)
-            {
-                BinaryTreeNode current = stack.Peek();
-                // we are traversing down the tree
-                if (previous == null || previous.LeftChild == current || previous.RightChild == current)
-                {
-                    if (current.LeftChild != null) stack.Push(current.LeftChild);
-                    else if (current.RightChild != null) stack.Push(current.RightChild);
-                }
-                // we are traversing up the tree from the left
-                else if (current.LeftChild == previous)
-                {
-                    if (current.RightChild != null)
-                    {
-                        stack.Push(current.RightChild);
-                    }
-                }
-                // we are traversing up the tree from the right
-                else
-                {
-                    stack.Pop();
-                }
-                previous = current;
-                if (stack.Count > maxDepth) maxDepth = stack.Count;
-            }
-            return maxDepth;
+            var leftDepth = FindBinaryTreeDepth(topNode.LeftChild);
+            var rightDepth = FindBinaryTreeDepth(topNode.RightChild);
+            return Math.Max(leftDepth + 1, rightDepth + 1);
         }
     }
 }
