0ac3a3f5babca0f8b20b70e4936f7d923df3a0f5
George Pavlovych <pavlheo@hotmail.com>
2016-04-16 13:41:09 +0300

    Fixed https://github.com/gpavlovych/Training/issues/3 and https://github.com/gpavlovych/Training/issues/4 input array is validated to be in [1..N] range; unit tests for fully unsorted array, sorted array, array w/o duplicates added

--- AkvelonMicrosoftTraining/Task39/Solver.cs
+++ AkvelonMicrosoftTraining/Task39/Solver.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.IO;
+    using System.Runtime.CompilerServices;
 
     /// <summary>
     /// Given an array of size N in which every number is between 1 and N, determine if there are any duplicates in it and what kind.
@@ -22,9 +23,22 @@
             }
             for (var i = 0; i < input.Length; i++)
             {
-                if (input[Math.Abs(input[i])] > 0)
+                if (input[i] < 1 || input[i] > input.Length)
                 {
-                    input[Math.Abs(input[i])] = -input[Math.Abs(input[i])];
+                    throw new ArgumentOutOfRangeException(
+                        "input",
+                        string.Format(
+                            "invalid input[{0}] value ({1}): outside [1..{2}] range",
+                            i,
+                            input[i],
+                            input.Length));
+                }
+            }
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input[Math.Abs(input[i]) - 1] > 0)
+                {
+                    input[Math.Abs(input[i]) - 1] = -input[Math.Abs(input[i]) - 1];
                 }
                 else
                 {
--- AkvelonMicrosoftTraining/Task39Tests/SolverTests.cs
+++ AkvelonMicrosoftTraining/Task39Tests/SolverTests.cs
@@ -13,6 +13,59 @@
     public class Task39Tests
     {
         [TestMethod]
+        public void TestSolveWithoutDuplicates()
+        {
+            //arrange
+            var input = new[] { 1, 2, 3, 4, 5 };
+            var textWriterMock = new Mock<TextWriter>();
+
+            //act
+            Solver.Solve(textWriterMock.Object, input);
+
+            //assert
+            textWriterMock.Verify(
+                it => it.WriteLine(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<object>()),
+                Times.Never);
+        }
+
+        [TestMethod]
+        public void TestSolveFullySorted()
+        {
+            //arrange
+            var input = new[] { 1, 1, 2, 3, 5 };
+            var textWriterMock = new Mock<TextWriter>();
+
+            //act
+            Solver.Solve(textWriterMock.Object, input);
+
+            //assert
+            textWriterMock.Verify(it => it.WriteLine("Item {0} is occured again at position {1}", 1, 1));
+            //verify that above were the only calls
+            textWriterMock.Verify(
+                it => it.WriteLine(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<object>()),
+                Times.Exactly(1));
+        }
+
+        [TestMethod]
+        public void TestSolveFullyUnsorted()
+        {
+            //arrange
+            var input = new[] { 5, 5, 3, 1, 3 };
+            var textWriterMock = new Mock<TextWriter>();
+
+            //act
+            Solver.Solve(textWriterMock.Object, input);
+
+            //assert
+            textWriterMock.Verify(it => it.WriteLine("Item {0} is occured again at position {1}", 5, 1));
+            textWriterMock.Verify(it => it.WriteLine("Item {0} is occured again at position {1}", 3, 4));
+            //verify that above were the only calls
+            textWriterMock.Verify(
+                it => it.WriteLine(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<object>()),
+                Times.Exactly(2));
+        }
+
+        [TestMethod]
         public void TestSolve()
         {
             //arrange
@@ -25,6 +78,58 @@
             //assert
             textWriterMock.Verify(it => it.WriteLine("Item {0} is occured again at position {1}", 1, 1));
             textWriterMock.Verify(it => it.WriteLine("Item {0} is occured again at position {1}", 2, 4));
+            //verify that above were the only calls
+            textWriterMock.Verify(
+                it => it.WriteLine(It.IsAny<string>(), It.IsAny<object>(), It.IsAny<object>()),
+                Times.Exactly(2));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestSolveInputInvalidItemNegativeExceedsLength()
+        {
+            //arrange
+            var input = new[] { 1, -5 };
+            var textWriterMock = new Mock<TextWriter>();
+
+            //act
+            Solver.Solve(textWriterMock.Object, input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestSolveInputInvalidItemPositiveExceedsLength()
+        {
+            //arrange
+            var input = new[] { 1, 5 };
+            var textWriterMock = new Mock<TextWriter>();
+
+            //act
+            Solver.Solve(textWriterMock.Object, input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestSolveInputInvalidItemNegative()
+        {
+            //arrange
+            var input = new[] { 1, -1 };
+            var textWriterMock = new Mock<TextWriter>();
+
+            //act
+            Solver.Solve(textWriterMock.Object, input);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestSolveInputInvalidItem0()
+        {
+            //arrange
+            var input = new[] { 1, 0 };
+            var textWriterMock = new Mock<TextWriter>();
+
+            //act
+            Solver.Solve(textWriterMock.Object, input);
         }
 
         [TestMethod]
