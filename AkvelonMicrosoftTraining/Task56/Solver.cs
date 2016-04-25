94b2a0cb8253ba2f0e0b849528d117adc3f22606
George Pavlovych <pavlheo@hotmail.com>
2016-04-25 07:41:12 +0300

    Task56 of Week2 implemented

---
+++ AkvelonMicrosoftTraining/Task56/Solver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task56
+{
+    public static class Solver
+    {
+        public static IEnumerable<int> FindDuplicates(int[] input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            if (input.Length == 0)
+            {
+                yield break;
+            }
+            for (var i = 1; i < input.Length; i++)
+            {
+                if (input[ i - 1 ] > input[ i ])
+                {
+                    throw new ArgumentException("Array is not sorted");
+                }
+            }
+            var previous = input[ 0 ];
+            for (var i = 1; i < input.Length; i++)
+            {
+                if (previous == input[ i ])
+                {
+                    yield return input[i];
+                }
+
+                else
+                {
+                    previous = input[ i ];
+                }
+            }
+        }
+    }
+}
