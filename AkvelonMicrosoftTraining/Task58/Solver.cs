540272df97ceb7108775225f7be76a785e689b0a
George Pavlovych <pavlheo@hotmail.com>
2016-04-25 10:01:23 +0300

    Task 58 of week2 implemented

---
+++ AkvelonMicrosoftTraining/Task58/Solver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task58
+{
+    /// <summary>
+    /// 58.Given an array of length N containing integers between 1 and N, 
+    /// determine if it contains any duplicates.
+    /// </summary>
+    public static class Solver
+    {
+        public static bool ExistDuplicates(int[] input)
+        {
+            #region Input validation
+
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input[ i ] < 1 || input[ i ] > input.Length)
+                {
+                    throw new ArgumentException("Array items should be in range [1..ArrayLength]");
+                }
+            }
+
+            #endregion Input validation
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                if (input[ Math.Abs(input[ i ]) - 1 ] > 0)
+                {
+                    input[ Math.Abs(input[ i ]) - 1 ] = -input[ Math.Abs(input[ i ]) - 1 ];
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Task58Tests/SolverTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Task58.Tests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void ExistDuplicatesTestExistDuplicates()
+        {
+            //arrange
+            var input = new int[]
+                            {
+                                1,
+                                2,
+                                4,
+                                2
+                            };
+
+            //act
+            var result = Solver.ExistDuplicates(input);
+
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ExistDuplicatesTestNoDuplicates()
+        {
+            //arrange
+            var input = new int[]
+                            {
+                                1,
+                                2,
+                                4,
+                                3
+                            };
+
+            //act
+            var result = Solver.ExistDuplicates(input);
+
+            //assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ExistDuplicatesTestEmpty()
+        {
+            //arrange
+            var input = new int[]
+                            {
+                            };
+
+            //act
+            var result = Solver.ExistDuplicates(input);
+
+            //assert
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExistDuplicatesTestNull()
+        {
+            Solver.ExistDuplicates(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ExistDuplicatesTestItemBelow1()
+        {
+            Solver.ExistDuplicates(
+                new int[]
+                    {
+                        0
+                    });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ExistDuplicatesTestItemExceedsLength()
+        {
+            Solver.ExistDuplicates(
+                new int[]
+                    {
+                        2
+                    });
+        }
+    }
+}
