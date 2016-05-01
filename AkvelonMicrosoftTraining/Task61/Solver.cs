bbe8e87df56b0330b117431f444ca68a0a2fce0e
George Pavlovych <pavlheo@hotmail.com>
2016-05-01 21:06:59 +0300

    Task 61 of week 2 completed

---
+++ AkvelonMicrosoftTraining/Task61/Solver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task61
+{
+    /// <summary>
+    /// 61.An array of pointers to (very long) strings. Find pointers to the (lexicographically) smallest and largest strings.
+    /// </summary>
+    public static class Solver
+    {
+        public static void FindMinMax(string[] input, out string min, out string max)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            if (input.Length == 0)
+            {
+                throw new ArgumentException("input should not be empty");
+            }
+            for (var index = 0; index < input.Length; index++)
+            {
+                if (input[index] == null)
+                {
+                    throw new ArgumentException(string.Format("input[{0}] should not be null", index));
+                }
+            }
+            min = input[0];
+            max = input[0];
+            for (var index = 1; index < input.Length; index++)
+            {
+                var currentString = input[index];
+                if (string.Compare(currentString, max, StringComparison.Ordinal) > 0)
+                {
+                    max = currentString;
+                }
+                if (string.Compare(currentString, min, StringComparison.Ordinal) < 0)
+                {
+                    min = currentString;
+                }
+            }
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Task61Tests/SolverTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Task61.Tests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void FindMinMaxTestDoubleItems()
+        {
+            //arrange
+            string min;
+            string max;
+
+            //act
+            Solver.FindMinMax(
+                new string[]
+                    {
+                        "aaa",
+                        "aba"
+                    },
+                out min,
+                out max);
+
+            //assert
+            Assert.AreEqual("aaa", min);
+            Assert.AreEqual("aba", max);
+        }
+
+        [TestMethod]
+        public void FindMinMaxTestSingleItem()
+        {
+            string min;
+            string max;
+            Solver.FindMinMax(new string[] { "some" }, out min, out max);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindMinMaxTestSingleNullItem()
+        {
+            string min;
+            string max;
+            Solver.FindMinMax(
+                new string[]
+                    {
+                        null
+                    },
+                out min,
+                out max);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindMinMaxTestDoubleNullItem()
+        {
+            string min;
+            string max;
+            Solver.FindMinMax(
+                new string[]
+                    {
+                        null,
+                        null
+                    },
+                out min,
+                out max);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindMinMaxTestEmpty()
+        {
+            string min;
+            string max;
+            Solver.FindMinMax(new string[] { }, out min, out max);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FindMinMaxTestNull()
+        {
+            string min;
+            string max;
+            Solver.FindMinMax(null, out min, out max);
+        }
+    }
+}
