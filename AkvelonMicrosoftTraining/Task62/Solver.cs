24263dbb28040e9070d71e146c5f5da7f18c309e
George Pavlovych <pavlheo@hotmail.com>
2016-05-01 22:30:55 +0300

    Task 62 of Week 2 done

---
+++ AkvelonMicrosoftTraining/Task62/Solver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Task62
+{
+    /// <summary>
+    /// 62.Write a program to remove duplicates from a sorted array.
+    /// </summary>
+    public static class Solver
+    {
+        public static int RemoveDuplicates(int[] input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            var index = 0;
+            while (index < input.Length - 1 && input[ index ] == input[ index + 1 ])
+            {
+                index++;
+            }
+            var isAscending = index < input.Length - 1 && ( input[ index ] < input[ index + 1 ] );
+            for (var i = index; i < input.Length - 1; i++)
+            {
+                if (isAscending ^ ( input[ i ] <= input[ i + 1 ] ))
+                    //all items of an input array should satisfy input[i]<=input[i+1] (it is sorted in ascending order) 
+                    //or should satisfy input[i]>=input[i+1] (descending one)
+                {
+                    throw new ArgumentException("Array is not sorted");
+                }
+            }
+            if (input.Length == 0)
+            {
+                return 0;
+            }
+            var currentIndex = 0;
+            for (var i = 1; i < input.Length && currentIndex < input.Length; i++)
+            {
+                if (input[currentIndex] != input[i])
+                {
+                    currentIndex++;
+                    input[currentIndex] = input[i];
+                }
+            }
+            return currentIndex + 1;
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Task62Tests/SolverTests.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace Task62.Tests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void RemoveDuplicatesTestDescending()
+        {
+            RemoveDuplicatesTest(
+                new[]
+                    {
+                        3,
+                        2,
+                        1
+                    });
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesTestAscending()
+        {
+            RemoveDuplicatesTest(
+                new[]
+                    {
+                        1,
+                        2,
+                        3
+                    });
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesTestAllThesame()
+        {
+            RemoveDuplicatesTest(
+                new[]
+                    {
+                        2,
+                        2,
+                        2,
+                        2,
+                        2
+                    });
+        }
+
+        [TestMethod]
+        public void RemoveDuplicatesTestDuplicates()
+        {
+            RemoveDuplicatesTest(
+                new[]
+                    {
+                        2,
+                        2,
+                        2,
+                        2,
+                        2,
+                        3,
+                        3,
+                        3,
+                        3,
+                        5
+                    });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RemoveDuplicatesTestNotSorted()
+        {
+            Solver.RemoveDuplicates(
+                new int[]
+                    {
+                        2,
+                        2,
+                        2,
+                        2,
+                        3,
+                        2
+                    });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RemoveDuplicatesTestNotSorted2()
+        {
+            Solver.RemoveDuplicates(
+                new int[]
+                    {
+                        2,
+                        2,
+                        2,
+                        2,
+                        3,
+                        3,
+                        3,
+                        3,
+                        3,
+                        3,
+                        3,
+                        3,
+                        2,
+                        2,
+                        2,
+                        2,
+                        2,
+                        1,
+                        2,
+                        2,
+                        3,
+                        2
+                    });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RemoveDuplicatesTestNull()
+        {
+            Solver.RemoveDuplicates(null);
+        }
+
+        private static void RemoveDuplicatesTest(int[] initialInput)
+        {
+            var input = (int[]) initialInput.Clone();
+            var expectedOutput = initialInput.Distinct().ToArray();
+
+            //act
+            var length = Solver.RemoveDuplicates(input);
+
+            //assert
+            Assert.AreEqual(expectedOutput.Length, length);
+            for (var i = 0; i < length; i++)
+            {
+                Assert.AreEqual(expectedOutput[ i ], input[ i ]);
+            }
+        }
+    }
+}
