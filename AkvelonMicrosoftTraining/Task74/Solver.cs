559b823146a39b24a53de1aa1084e126506d5b5f
George Pavlovych <pavlheo@hotmail.com>
2016-05-03 23:25:23 +0300

    Task 74 of week2 done

---
+++ AkvelonMicrosoftTraining/Task74/Solver.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Task74
+{
+    public static class Solver
+    {
+        public static int? FindItemMoreN2(int[] input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            /* Initially, we have no guess and our count is zero.  However, to avoid
+              * edge cases with the empty range, we initialize the candidate to null.
+              */
+            int? candidate = null;
+            int confidence = 0;
+
+            /* Scan over the input using the Boyer-Moore update rules. */
+            foreach (var inputItem in input)
+            {
+                /* If we have no confidence in our previous guess, update it to this new
+                 * element.
+                 */
+                if (confidence == 0)
+                {
+                    candidate = inputItem;
+                    ++confidence;
+                }
+                /* Otherwise, increment or decrement the confidence based on whether the
+                 * next element matches.
+                 */
+                else if (candidate==inputItem)
+                    ++confidence;
+                else
+                    --confidence;
+            }
+
+            /* Finally, do one more pass to confirm that we have a majority element. */
+            int numMatches = 0, totalElements = 0;
+            foreach (var inputItem in input)
+            {
+                /* Check whether this is a match and update appropriately. */
+                if (candidate == inputItem)
+                    ++numMatches;
+
+                /* Either way, increment the total number of elements. */
+                ++totalElements;
+            }
+
+            /* This is a majority element if it accounts for at least half the number
+             * of elements.
+             */
+            return totalElements / 2 < numMatches ? candidate : null;
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Task74Tests/SolverTests.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Task74.Tests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void FindItemMoreN2TestTripleSame()
+        {
+            Assert.AreEqual(
+                1,
+                Solver.FindItemMoreN2(
+                    new int[]
+                        {
+                            1,
+                            1,
+                            1
+                        }));
+        }
+
+        [TestMethod]
+        public void FindItemMoreN2TestTripleMajority()
+        {
+            Assert.AreEqual(
+                1,
+                Solver.FindItemMoreN2(
+                    new int[]
+                        {
+                            1,
+                            1,
+                            2
+                        }));
+        }
+
+        [TestMethod]
+        public void FindItemMoreN2TestTripleUnique()
+        {
+            Assert.IsNull(
+                Solver.FindItemMoreN2(
+                    new int[]
+                        {
+                            1,
+                            2,
+                            3
+                        }));
+        }
+
+        [TestMethod]
+        public void FindItemMoreN2TestDoubleSame()
+        {
+            Assert.AreEqual(
+                1,
+                Solver.FindItemMoreN2(
+                    new int[]
+                        {
+                            1,
+                            1
+                        }));
+        }
+
+        [TestMethod]
+        public void FindItemMoreN2TestDoubleUnique()
+        {
+            Assert.IsNull(
+                Solver.FindItemMoreN2(
+                    new int[]
+                        {
+                            1,
+                            2
+                        }));
+        }
+
+        [TestMethod]
+        public void FindItemMoreN2TestSingle()
+        {
+            Assert.AreEqual(
+                3,
+                Solver.FindItemMoreN2(
+                    new int[]
+                        {
+                            3
+                        }));
+        }
+
+        [TestMethod]
+        public void FindItemMoreN2TestEmpty()
+        {
+            Assert.IsNull(
+                Solver.FindItemMoreN2(
+                    new int[]
+                        {
+                        }));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FindItemMoreN2TestNull()
+        {
+            Solver.FindItemMoreN2(null);
+        }
+    }
+}
