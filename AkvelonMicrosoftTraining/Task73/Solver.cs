bc53703e08071952406bc1d447393675b58585c5
George Pavlovych <pavlheo@hotmail.com>
2016-05-03 22:57:49 +0300

    Task 73 of week2 implemented

---
+++ AkvelonMicrosoftTraining/Task73/Solver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task73
+{
+    /// <summary>
+    /// 73. Check string for palindrome. Palindrome is: mam, toyot, mm, a and so on. 
+    /// The string you can read in both directions.
+    /// </summary>
+    public static class Solver
+    {
+        public static bool IsPalindrome(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            var result = true;
+            for (var i = 0; i < input.Length / 2; i++)
+            {
+                if (input[ i ] != input[ input.Length - i -1])
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Task73Tests/SolverTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task73.Tests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void IsPalindromeTestToyot()
+        {
+            Assert.IsTrue(Solver.IsPalindrome("toyot"));
+        }
+
+        [TestMethod]
+        public void IsPalindromeTestToyota()
+        {
+            Assert.IsFalse(Solver.IsPalindrome("toyota"));
+        }
+
+        [TestMethod]
+        public void IsPalindromeTestMam()
+        {
+            Assert.IsTrue(Solver.IsPalindrome("mam"));
+        }
+
+        [TestMethod]
+        public void IsPalindromeTestMm()
+        {
+            Assert.IsTrue(Solver.IsPalindrome("mm"));
+        }
+
+        [TestMethod]
+        public void IsPalindromeTestA()
+        {
+            Assert.IsTrue(Solver.IsPalindrome("a"));
+        }
+
+        [TestMethod]
+        public void IsPalindromeTestAb()
+        {
+            Assert.IsFalse(Solver.IsPalindrome("ab"));
+        }
+
+        [TestMethod]
+        public void IsPalindromeTestEmpty()
+        {
+            Assert.IsTrue(Solver.IsPalindrome(""));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void IsPalindromeTestNull()
+        {
+            Solver.IsPalindrome(null);
+        }
+    }
+}
