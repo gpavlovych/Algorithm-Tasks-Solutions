b31d0f548efb8dc45911b3e8fc3601dcdf867807
George Pavlovych <pavlheo@hotmail.com>
2016-05-03 14:21:50 +0300

    Task69 of Week2 done

---
+++ AkvelonMicrosoftTraining/Task69/Solver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task69
+{
+    /// <summary>
+    /// 69. Compute the discrete log of an unsigned integer.
+    /// </summary>
+    public static class Solver
+    {
+        public static int DiscreteLog(uint a, ulong b)
+        {
+            if (a <= 1)
+            {
+                throw new ArgumentOutOfRangeException("a");
+            }
+            int k = 0;
+            ulong pow = 1u;
+            while (pow <= b)
+            {
+                pow *= a;
+                k++;
+            }
+            return k - 1;
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Task69Tests/SolverTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Task69.Tests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void DiscreteLogTestBase10Argument11()
+        {
+            Assert.AreEqual(1, Solver.DiscreteLog(10, 11));
+        }
+
+        [TestMethod]
+        public void DiscreteLogTestBase10Argument9()
+        {
+            Assert.AreEqual(0, Solver.DiscreteLog(10, 9));
+        }
+
+        [TestMethod]
+        public void DiscreteLogTestBase10Argument1()
+        {
+            Assert.AreEqual(0, Solver.DiscreteLog(10, 1));
+        }
+
+        [TestMethod]
+        public void DiscreteLogTestBase2Argument35()
+        {
+            Assert.AreEqual(5, Solver.DiscreteLog(2, 35));
+        }
+
+        [TestMethod]
+        public void DiscreteLogTestBase2Argument32()
+        {
+            Assert.AreEqual(5, Solver.DiscreteLog(2, 32));
+        }
+
+        [TestMethod]
+        public void DiscreteLogTestBase2Argument4()
+        {
+            Assert.AreEqual(2, Solver.DiscreteLog(2, 4));
+        }
+
+        [TestMethod]
+        public void DiscreteLogTestBase2Argument3()
+        {
+            Assert.AreEqual(1, Solver.DiscreteLog(2, 3));
+        }
+
+        [TestMethod]
+        public void DiscreteLogTestBase2Argument2()
+        {
+            Assert.AreEqual(1, Solver.DiscreteLog(2, 2));
+        }
+
+        [TestMethod]
+        public void DiscreteLogTestBase2Argument1()
+        {
+            Assert.AreEqual(0, Solver.DiscreteLog(2, 1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DiscreteLogTestBase1()
+        {
+            Assert.AreEqual(0, Solver.DiscreteLog(1, 1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DiscreteLogTestBase0()
+        {
+            Assert.AreEqual(0, Solver.DiscreteLog(0, 1));
+        }
+    }
+}
