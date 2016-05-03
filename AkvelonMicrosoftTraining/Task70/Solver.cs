add8b790cf32504396681c324e102d9756b793e2
George Pavlovych <pavlheo@hotmail.com>
2016-05-03 14:33:27 +0300

    Task 70 of Week 2 done

---
+++ AkvelonMicrosoftTraining/Task70/Solver.cs
@@ -0,0 +1,13 @@
+﻿namespace Task70
+{
+    /// <summary>
+    /// 70. Set the highest significant bit of an unsigned integer to zero.
+    /// </summary>
+    public static class Solver
+    {
+        public static uint ClearHighestSignificantBit(uint num)
+        {
+            return ( num << 1 ) >> 1;
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Task70Tests/SolverTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task70.Tests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void ClearHighestSignificantBitTest()
+        {
+            Assert.AreEqual(0x7FFFFFFFu, Solver.ClearHighestSignificantBit(0xFFFFFFFFu));
+        }
+
+        [TestMethod]
+        public void ClearHighestSignificantBitTestAlreadyClear()
+        {
+            Assert.AreEqual(0x7FFFFFFFu, Solver.ClearHighestSignificantBit(0x7FFFFFFFu));
+        }
+
+        [TestMethod]
+        public void ClearHighestSignificantBitTestZero()
+        {
+            Assert.AreEqual(0u, Solver.ClearHighestSignificantBit(0u));
+        }
+    }
+}
