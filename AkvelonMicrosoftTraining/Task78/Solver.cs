f693b6d96690dbeab766d6a4ad015e49d2897516
George Pavlovych <pavlheo@hotmail.com>
2016-05-04 02:01:09 +0300

    Task78 of week2 done

---
+++ AkvelonMicrosoftTraining/Task78/Solver.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+
+namespace Task78
+{
+    /// <summary>
+    /// 78.Write a program that prints Fibonacci series up to N number.
+    /// </summary>
+    public static class Solver
+    {
+        public static void PrintFibonacci(TextWriter writer, int n)
+        {
+            var f1 = 1; // item before previous
+            var f2 = 1; // previous item
+            if (n >= 1)
+            {
+                writer.Write("{0} ", f1);
+            }
+            if (n >= 2)
+            {
+                writer.Write("{0} ", f2);
+            }
+            for (var i = 3; i <= n; i++)
+            {
+                var f = f1 + f2;
+                writer.Write("{0} ", f);
+                f2 = f1;
+                f1 = f;
+            }
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Task78Tests/SolverTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text;
+using Moq;
+
+namespace Task78.Tests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        private StringBuilder result;
+        private Mock<TextWriter> textWriterMock;
+
+        [TestInitialize]
+        public void Init()
+        {
+            //arrange
+            result = new StringBuilder();
+            textWriterMock = new Mock<TextWriter>();
+            textWriterMock.Setup(it => it.Write(It.IsAny<string>(), It.IsAny<object>()))
+                          .Callback<string, object>((str, obj) => result.Append(string.Format(str, obj)));
+        }
+
+        [TestMethod]
+        public void PrintFibonacciTest0()
+        {
+            //act
+            Solver.PrintFibonacci(textWriterMock.Object, 0);
+
+            //assert
+            Assert.AreEqual("", result.ToString());
+        }
+
+        [TestMethod]
+        public void PrintFibonacciTest1()
+        {
+            //act
+            Solver.PrintFibonacci(textWriterMock.Object, 1);
+
+            //assert
+            Assert.AreEqual("1 ", result.ToString());
+        }
+
+        [TestMethod]
+        public void PrintFibonacciTest2()
+        {
+            //act
+            Solver.PrintFibonacci(textWriterMock.Object, 2);
+
+            //assert
+            Assert.AreEqual("1 1 ", result.ToString());
+        }
+
+        [TestMethod]
+        public void PrintFibonacciTest3()
+        {
+            //act
+            Solver.PrintFibonacci(textWriterMock.Object, 3);
+
+            //assert
+            Assert.AreEqual("1 1 2 ", result.ToString());
+        }
+
+        [TestMethod]
+        public void PrintFibonacciTest4()
+        {
+            //act
+            Solver.PrintFibonacci(textWriterMock.Object, 4);
+
+            //assert
+            Assert.AreEqual("1 1 2 3 ", result.ToString());
+        }
+
+        [TestMethod]
+        public void PrintFibonacciTest5()
+        {
+            //act
+            Solver.PrintFibonacci(textWriterMock.Object, 5);
+
+            //assert
+            Assert.AreEqual("1 1 2 3 5 ", result.ToString());
+        }
+    }
+}
