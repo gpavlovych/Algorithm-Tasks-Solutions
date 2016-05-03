4899b62bc8ba5362095ca3695db87da5f71f9237
George Pavlovych <pavlheo@hotmail.com>
2016-05-04 01:45:08 +0300

    Task 77 of week 2 implemented

---
+++ AkvelonMicrosoftTraining/Task77/Solver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task77
+{
+    /// <summary>
+    /// 77.Write a program that prints the numbers from 1 to 100. But for multiples of three prints “Fizz” 
+    /// instead of the number and for the multiples of five prints “Buzz”. 
+    /// For numbers, which are, multiples of both three and five prints “FizzBuzz”. How would you test it!
+    /// </summary>
+    public static class Solver
+    {
+        public static void PrintFizzBuzz(TextWriter writer)
+        {
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    writer.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    writer.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    writer.WriteLine("Buzz");
+                }
+                else
+                    writer.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/Task77Tests/SolverTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Task77.Tests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void PrintFizzBuzzTestStringWriter()
+        {
+            //arrange
+            var result = new StringBuilder();
+            using (var writer = new StringWriter(result))
+            {
+                //act
+                Solver.PrintFizzBuzz(writer);
+
+                //assert
+                AssertOutput(result);
+            }
+        }
+
+        [TestMethod]
+        public void PrintFizzBuzzTestMoq()
+        {
+            //arrange
+            var result = new StringBuilder();
+            var textWriterMock = new Mock<TextWriter>();
+            textWriterMock.Setup(it => it.WriteLine(It.IsAny<string>())).Callback<string>(str => result.AppendLine(str));
+            textWriterMock.Setup(it => it.WriteLine(It.IsAny<int>()))
+                          .Callback<int>(num => result.AppendLine(num.ToString()));
+
+            //act
+            Solver.PrintFizzBuzz(textWriterMock.Object);
+
+            //assert
+            AssertOutput(result);
+        }
+
+        #region Helper methods
+
+        private static void AssertOutput(StringBuilder result)
+        {
+            var numbers = result.ToString().Split(
+                new[]
+                    {
+                        "\n"
+                    },
+                StringSplitOptions.RemoveEmptyEntries).Select(it => it.Trim()).ToArray();
+            Assert.AreEqual(100, numbers.Length);
+            for (var i = 1; i <= numbers.Length; i++)
+            {
+                if (i % 3 != 0 && i % 5 != 0)
+                {
+                    Assert.AreEqual(i.ToString(), numbers[ i - 1 ]);
+                }
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Assert.AreEqual("Fizz", numbers[ i - 1 ]);
+                }
+                if (i % 3 != 0 && i % 5 == 0)
+                {
+                    Assert.AreEqual("Buzz", numbers[ i - 1 ]);
+                }
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Assert.AreEqual("FizzBuzz", numbers[ i - 1 ]);
+                }
+            }
+        }
+
+        #endregion Helper methods
+    }
+}
