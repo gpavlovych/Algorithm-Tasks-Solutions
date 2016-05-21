c16c0a2766423e6c67bcb31ce3858cbe89123e86
George Pavlovych <pavlheo@hotmail.com>
2016-05-21 21:13:38 +0300

    Task template is added

---
+++ AkvelonMicrosoftTraining/TaskTemplate/TaskTemplateExtension/ProjectTemplates/TaskMultiproj/Task/Solver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// TODO put here the task description.
+    /// </summary>
+    /// <remarks>TODO put here answers of additional questions which help successfully solve the task.</remarks>
+    public static class Solver
+    {
+        public static object DoSomething(object input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+
+            return new object();
+        }
+    }
+}
