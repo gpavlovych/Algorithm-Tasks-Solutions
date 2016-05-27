7b4da0ebe2b053a81cffb782ae179ab69710ff50
George Pavlovych <pavlheo@hotmail.com>
2016-05-27 11:37:41 +0300

    Task 2 of Week3 solved

---
+++ AkvelonMicrosoftTraining/AkvelonMicrosoftTraining_Week3/Task2/Task2/Solver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task2
+{
+    /// <summary>
+    /// 2. Given a linked list find the N-th element from the end of the list.
+    /// </summary>
+    /// <remarks>TODO put here answers of additional questions which help successfully solve the task.</remarks>
+    public static class Solver
+    {
+        public static Node FindNthElementFromTheEnd(ref Node head, int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentOutOfRangeException("n", "must be between 0 and linked list size");
+            }
+            var currentNthNode = head;
+            for (var i = 0; i < n; i++)
+            {
+                if (currentNthNode == null)
+                {
+                    throw new ArgumentOutOfRangeException("n", "must be between 0 and linked list size");
+                }
+                currentNthNode = currentNthNode.Next;
+            }
+            var currentNode = head;
+            while (currentNthNode != null)
+            {
+                currentNthNode = currentNthNode.Next;
+                currentNode = currentNode.Next;
+            }
+            return currentNode;
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/AkvelonMicrosoftTraining_Week3/Task2/Task2Tests/SolverTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task2;
+
+namespace Task2Tests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void FindNthElementFromTheEnd3()
+        {
+            //arrange
+            var head = new Node(3)
+                           {
+                               Next = new Node(4)
+                                          {
+                                              Next = new Node(5)
+                                                         {
+                                                             Next = new Node(6)
+                                                         }
+                                          }
+                           };
+
+            //act
+            var result = Solver.FindNthElementFromTheEnd(ref head, 3);
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(4, result.Value);
+        }
+
+        [TestMethod]
+        public void FindNthElementFromTheEnd1()
+        {
+            //arrange
+            var head = new Node(3)
+                           {
+                               Next = new Node(4)
+                                          {
+                                              Next = new Node(5)
+                                                         {
+                                                             Next = new Node(6)
+                                                         }
+                                          }
+                           };
+
+            //act
+            var result = Solver.FindNthElementFromTheEnd(ref head, 1);
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(6, result.Value);
+        }
+
+        [TestMethod]
+        public void FindNthElementFromTheEnd0()
+        {
+            //arrange
+            var head = new Node(3)
+                           {
+                               Next = new Node(4)
+                                          {
+                                              Next = new Node(5)
+                                                         {
+                                                             Next = new Node(6)
+                                                         }
+                                          }
+                           };
+
+            //act
+            var result = Solver.FindNthElementFromTheEnd(ref head, 0);
+
+            //assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void FindNthElementFromTheEnd4()
+        {
+            //arrange
+            var head = new Node(3)
+                           {
+                               Next = new Node(4)
+                                          {
+                                              Next = new Node(5)
+                                                         {
+                                                             Next = new Node(6)
+                                                         }
+                                          }
+                           };
+
+            //act
+            var result = Solver.FindNthElementFromTheEnd(ref head, 4);
+
+            //assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Value);
+        }
+
+        [TestMethod]
+        public void FindNthElementFromTheEndEmptyList0()
+        {
+            //arrange
+            Node head = null;
+
+            //act
+            var result = Solver.FindNthElementFromTheEnd(ref head, 0);
+
+            //assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void FindNthElementFromTheEndHeadLessThan0()
+        {
+            //arrange
+            Node head = null;
+
+            //act
+            Solver.FindNthElementFromTheEnd(ref head, -1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void FindNthElementFromTheEndHeadExceedingLength()
+        {
+            //arrange
+            Node head = null;
+
+            //act
+            Solver.FindNthElementFromTheEnd(ref head, 1);
+        }
+    }
+}
