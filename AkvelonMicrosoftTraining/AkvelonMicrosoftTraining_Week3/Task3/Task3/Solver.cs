508211b1e0f5932043e304b61a59a2915a8fd022
George Pavlovych <pavlheo@hotmail.com>
2016-05-27 11:59:03 +0300

    Task3 of Week 3 done

---
+++ AkvelonMicrosoftTraining/AkvelonMicrosoftTraining_Week3/Task3/Task3/Solver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task3
+{
+    /// <summary>
+    /// 3. Write a function to calculate x in the power of y.
+    /// </summary>
+    /// <remarks>TODO put here answers of additional questions which help successfully solve the task.</remarks>
+    public static class Solver
+    {
+        public static float Power(float input, int n)
+        {
+            var result = 1.0f;
+            var p = 1;
+            while (n != 0)
+            {
+                var t = n % 2;
+                n = n / 2;
+                if (t == 1)
+                {
+                    result *= input;
+                }
+                input *= input;
+            }
+            return result;
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/AkvelonMicrosoftTraining_Week3/Task3/Task3Tests/SolverTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task3;
+
+namespace Task3Tests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void PowerTest_1_0_1()
+        {
+
+            Assert.AreEqual(1, Solver.Power(1, 0));
+        }
+
+        [TestMethod]
+        public void PowerTest_1_1_1()
+        {
+
+            Assert.AreEqual(1, Solver.Power(1, 1));
+        }
+
+        [TestMethod]
+        public void PowerTest_2_1_2()
+        {
+
+            Assert.AreEqual(2, Solver.Power(2, 1));
+        }
+
+        [TestMethod]
+        public void PowerTest_2_10_1024()
+        {
+
+            Assert.AreEqual(1024, Solver.Power(2, 10));
+        }
+    }
+}
