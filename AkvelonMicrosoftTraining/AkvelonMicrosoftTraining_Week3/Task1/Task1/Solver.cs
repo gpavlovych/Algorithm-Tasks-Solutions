520047ecb28db39b60738524a46d56966621cde6
George Pavlovych <pavlheo@hotmail.com>
2016-05-27 11:03:01 +0300

    Task 1 of week3 is solved

---
+++ AkvelonMicrosoftTraining/AkvelonMicrosoftTraining_Week3/Task1/Task1/Solver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.CodeDom;
+using System.Runtime.ExceptionServices;
+
+namespace Task1
+{
+    /// <summary>
+    /// 1. In 2-D array, find a zero and replace the entire row and column with zeros.
+    /// </summary>
+    /// <remarks>TODO put here answers of additional questions which help successfully solve the task.</remarks>
+    public static class Solver
+    {
+        public static void ReplaceColumnsAndRowsIfZeroFound(int[,] input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            var rowCount = input.GetLength(0);
+            var columnCount = input.GetLength(1);
+            var row = new bool[rowCount];
+            var column = new bool[columnCount];
+
+            for (var rowIndex = 0; rowIndex < rowCount; rowIndex++)
+            {
+                for (var columnIndex = 0; columnIndex < columnCount; columnIndex++)
+                {
+                    if (input[ rowIndex, columnIndex ] == 0)
+                    {
+                        row[ rowIndex ] = column[ columnIndex ] = true;
+                    }
+                }
+            }
+
+            for (var rowIndex = 0; rowIndex < rowCount; rowIndex++)
+            {
+                if (row[ rowIndex ])
+                    for (int columnIndex = 0; columnIndex < columnCount; columnIndex++)
+                    {
+                        input[ rowIndex, columnIndex ] = 0;
+                    }
+            }
+            for (int columnIndex = 0; columnIndex < columnCount; columnIndex++)
+            {
+                if (column[ columnIndex ])
+                    for (var rowIndex = 0; rowIndex < rowCount; rowIndex++)
+                    {
+                        input[ rowIndex, columnIndex ] = 0;
+                    }
+            }
+        }
+    }
+}
---
+++ AkvelonMicrosoftTraining/AkvelonMicrosoftTraining_Week3/Task1/Task1Tests/SolverTests.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Linq;
+using BaseTests.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task1;
+
+namespace Task1Tests
+{
+    [TestClass]
+    public class SolverTests
+    {
+        [TestMethod]
+        public void ReplaceColumnsAndRowsIfZeroFoundOneZero23()
+        {
+            //arrange
+            var input = new int[,]
+                            {
+                                {
+                                    1,
+                                    2
+                                },
+                                {
+                                    4,
+                                    0
+                                },
+                                {
+                                    7,
+                                    8
+                                }
+                            };
+            var expectedOutput = new int[,]
+                                     {
+                                         {
+                                             1,
+                                             0
+                                         },
+                                         {
+                                             0,
+                                             0
+                                         },
+                                         {
+                                             7,
+                                             0
+                                         }
+                                     };
+
+            //act
+            Solver.ReplaceColumnsAndRowsIfZeroFound(input);
+
+            //assert
+            TestHelper.AssertCollectionsEqual(expectedOutput.OfType<int>(), input.OfType<int>());
+        }
+
+        [TestMethod]
+        public void ReplaceColumnsAndRowsIfZeroFoundOneZero32()
+        {
+            //arrange
+            var input = new int[,]
+                            {
+                                {
+                                    1,
+                                    2,
+                                    3
+                                },
+                                {
+                                    4,
+                                    0,
+                                    6
+                                }
+                            };
+            var expectedOutput = new int[,]
+                                     {
+                                         {
+                                             1,
+                                             0,
+                                             3
+                                         },
+                                         {
+                                             0,
+                                             0,
+                                             0
+                                         }
+                                     };
+
+            //act
+            Solver.ReplaceColumnsAndRowsIfZeroFound(input);
+
+            //assert
+            TestHelper.AssertCollectionsEqual(expectedOutput.OfType<int>(), input.OfType<int>());
+        }
+
+        [TestMethod]
+        public void ReplaceColumnsAndRowsIfZeroFoundOneZero()
+        {
+            //arrange
+            var input = new int[,]
+                            {
+                                {
+                                    1,
+                                    2,
+                                    3
+                                },
+                                {
+                                    4,
+                                    0,
+                                    6
+                                },
+                                {
+                                    7,
+                                    8,
+                                    9
+                                }
+                            };
+            var expectedOutput = new int[,]
+                                     {
+                                         {
+                                             1,
+                                             0,
+                                             3
+                                         },
+                                         {
+                                             0,
+                                             0,
+                                             0
+                                         },
+                                         {
+                                             7,
+                                             0,
+                                             9
+                                         }
+                                     };
+
+            //act
+            Solver.ReplaceColumnsAndRowsIfZeroFound(input);
+
+            //assert
+            TestHelper.AssertCollectionsEqual(expectedOutput.OfType<int>(), input.OfType<int>());
+        }
+
+        [TestMethod]
+        public void ReplaceColumnsAndRowsIfZeroFoundNoZero()
+        {
+            //arrange
+            var input = new int[,]
+                            {
+                                {
+                                    1,
+                                    2,
+                                    3
+                                },
+                                {
+                                    4,
+                                    5,
+                                    6
+                                },
+                                {
+                                    7,
+                                    8,
+                                    9
+                                }
+                            };
+            var expectedOutput = new int[,]
+                                     {
+                                         {
+                                             1,
+                                             2,
+                                             3
+                                         },
+                                         {
+                                             4,
+                                             5,
+                                             6
+                                         },
+                                         {
+                                             7,
+                                             8,
+                                             9
+                                         }
+                                     };
+
+            //act
+            Solver.ReplaceColumnsAndRowsIfZeroFound(input);
+
+            //assert
+            TestHelper.AssertCollectionsEqual(expectedOutput.OfType<int>(), input.OfType<int>());
+        }
+
+        [TestMethod]
+        public void ReplaceColumnsAndRowsIfZeroFoundTwoEmptyRows()
+        {
+            //act
+            Solver.ReplaceColumnsAndRowsIfZeroFound(new int[,] { {}, {} } );
+        }
+
+        [TestMethod]
+        public void ReplaceColumnsAndRowsIfZeroFoundSingleEmptyRow()
+        {
+            //act
+            Solver.ReplaceColumnsAndRowsIfZeroFound(new int[,] { {} });
+        }
+
+        [TestMethod]
+        public void ReplaceColumnsAndRowsIfZeroFoundEmpty()
+        {
+             //act
+            Solver.ReplaceColumnsAndRowsIfZeroFound(new int[,] { });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void DoSomethingNullInput()
+        {
+            //act
+            Solver.ReplaceColumnsAndRowsIfZeroFound(null);
+        }
+    }
+}
