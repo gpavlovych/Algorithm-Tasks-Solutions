3abdd36e6648aae2572018faf7a40c0dfc0a0985
Pavlovych George <pavlheo@hotmail.com>
2017-09-22 13:34:23 +0300

    Task 1 of Intensified task list solved

--- AkvelonMicrosoftTraining/Intensified/Task1/Task1/Solver.cs
+++ AkvelonMicrosoftTraining/Intensified/Task1/Task1/Solver.cs
@@ -5,10 +5,32 @@ using System.Runtime.ExceptionServices;
 namespace Task1
 {
     /// <summary>
-    /// 1. Given f(n) = n⁄2, when n is even and f(n)=f(3n+1), when n is odd. Write recursive function to compute f(n)
+    /// 1. Given f(n) = n⁄2, when n is even and f(n)=f(3n+1), when n is odd. Write recursive function to compute f(n).
     /// </summary>
     public static class Solver
     {
-        
+        public static int RecFunc(int n)
+        {
+            if (n % 2 != 0)
+            {
+                //n is odd,
+                //downstream n will become even (always, 3n+1 for even number, even number can be represented as n=2k+1, where k is integer => 3(2k+1)+1 = 6k+4 - always even)
+                return RecFunc(3 * n + 1);
+            }
+
+            return n / 2;
+        }
+
+        public static int PlainFunc(int n)
+        {
+            if (n % 2 != 0)
+            {
+                //n is odd,
+                //downstream n will become even
+                return (3 * n + 1)/2;
+            }
+
+            return n / 2;
+        }
     }
 }
--- AkvelonMicrosoftTraining/Intensified/Task1/Task1Tests/SolverTests.cs
+++ AkvelonMicrosoftTraining/Intensified/Task1/Task1Tests/SolverTests.cs
@@ -8,5 +8,106 @@ namespace Task1Tests
     [TestClass]
     public class SolverTests
     {
+        [TestMethod]
+        public void RecFuncTest0()
+        {
+            this.RecFuncTest(0, 0);
+        }
+
+        [TestMethod]
+        public void RecFuncTest1()
+        {
+            this.RecFuncTest(2, 1);
+        }
+
+        [TestMethod]
+        public void RecFuncTest2()
+        {
+            this.RecFuncTest(1, 2);
+        }
+
+        [TestMethod]
+        public void RecFuncTest3()
+        {
+            this.RecFuncTest(5, 3);
+        }
+
+        [TestMethod]
+        public void RecFuncTestNeg1()
+        {
+            this.RecFuncTest(-1, -1);
+        }
+
+        [TestMethod]
+        public void RecFuncTestNeg2()
+        {
+            this.RecFuncTest(-1, -2);
+        }
+
+        [TestMethod]
+        public void RecFuncTestNeg3()
+        {
+            this.RecFuncTest(-4, -3);
+        }
+
+        [TestMethod]
+        public void PlainFuncTest0()
+        {
+            this.PlainFuncTest(0, 0);
+        }
+
+        [TestMethod]
+        public void PlainFuncTest1()
+        {
+            this.PlainFuncTest(2, 1);
+        }
+
+        [TestMethod]
+        public void PlainFuncTest2()
+        {
+            this.PlainFuncTest(1, 2);
+        }
+
+        [TestMethod]
+        public void PlainFuncTest3()
+        {
+            this.PlainFuncTest(5, 3);
+        }
+
+        [TestMethod]
+        public void PlainFuncTestNeg1()
+        {
+            this.PlainFuncTest(-1, -1);
+        }
+
+        [TestMethod]
+        public void PlainFuncTestNeg2()
+        {
+            this.PlainFuncTest(-1, -2);
+        }
+
+        [TestMethod]
+        public void PlainFuncTestNeg3()
+        {
+            this.PlainFuncTest(-4, -3);
+        }
+
+        private void RecFuncTest(int expectedResult, int arg)
+        {
+            //act
+            var result = Solver.RecFunc(arg);
+
+            //assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        private void PlainFuncTest(int expectedResult, int arg)
+        {
+            //act
+            var result = Solver.PlainFunc(arg);
+
+            //assert
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
