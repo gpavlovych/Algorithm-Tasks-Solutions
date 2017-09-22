c70755daa394740116f64d2b4b2722c82e720aef
Pavlovych George <pavlheo@hotmail.com>
2017-09-22 19:01:35 +0300

    Task 3 of Intensified course done

--- AkvelonMicrosoftTraining/Intensified/Task3/Task3/Solver.cs
+++ AkvelonMicrosoftTraining/Intensified/Task3/Task3/Solver.cs
@@ -9,5 +9,43 @@ namespace Task3
     /// </summary>
     public static class Solver
     {
+        /// <summary>
+        /// Returns index of the item where sum (a[0..index]) = sum(a[index+1..N-1])
+        /// </summary>
+        /// <param name="inputs"></param>
+        /// <returns></returns>
+        public static int FindMedian(int [] inputs)
+        {
+            if (inputs == null)
+            {
+                throw new ArgumentNullException(nameof(inputs));
+            }
+
+            if (inputs.Length < 2)
+            {
+                return -1;
+            }
+
+            var leftSum = 0;
+            for (var inputIndex = 0; inputIndex < inputs.Length; inputIndex++)
+            {
+                leftSum += inputs[inputIndex];
+            }
+            
+            var rightSum = 0;
+            int rightIndex;
+            for (rightIndex = inputs.Length - 1; rightIndex >= 0; rightIndex--)
+            {
+                var currentItem = inputs[rightIndex];
+                rightSum += currentItem;
+                leftSum -= currentItem;
+                if (rightSum == leftSum)
+                {
+                    return rightIndex - 1;
+                }
+            }
+
+            return -1;
+        }
     }
 }
--- AkvelonMicrosoftTraining/Intensified/Task3/Task3Tests/SolverTests.cs
+++ AkvelonMicrosoftTraining/Intensified/Task3/Task3Tests/SolverTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Task3;
 
@@ -8,6 +7,56 @@ namespace Task3Tests
     [TestClass]
     public class SolverTests
     {
-      
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FindMedianNullTest()
+        {
+            this.FindMedianTest(-1, null);
+        }
+
+        [TestMethod]
+        public void FindMedianEmptyTest()
+        {
+            this.FindMedianTest(-1, new int[] { });
+        }
+
+        [TestMethod]
+        public void FindMedianSingleTest()
+        {
+            this.FindMedianTest(-1, new int[] {0});
+        }
+
+        [TestMethod]
+        public void FindMedianDoubleNonEqualTest()
+        {
+            this.FindMedianTest(-1, new int[] {0, 1});
+        }
+
+        [TestMethod]
+        public void FindMedianDoubleEqualTest()
+        {
+            this.FindMedianTest(0, new int[] {1, 1});
+        }
+
+        [TestMethod]
+        public void FindMedianMultipleNegativeTest()
+        {
+            this.FindMedianTest(-1, new int[] {1, 0, -11, 1, 12});
+        }
+
+        [TestMethod]
+        public void FindMedianMultiplePositiveTest()
+        {
+            this.FindMedianTest(1, new int[] { 1, 0, -11, 12 });
+        }
+
+        private void FindMedianTest(int expected, int[] inputs)
+        {
+            // act
+            var result = Solver.FindMedian(inputs);
+
+            // assert
+            Assert.AreEqual(expected, result);
+        }
     }
 }
