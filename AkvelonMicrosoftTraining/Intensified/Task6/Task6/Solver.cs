a8a9c7fcaf526361fc2daf9af5ccd8aa5d888ab2
EUROPE\v-hepav <v-hepav@microsoft.com>
2017-09-29 19:45:27 +0300

    Task 6 of Intensified course done

--- AkvelonMicrosoftTraining/Intensified/Task6/Task6/Solver.cs
+++ AkvelonMicrosoftTraining/Intensified/Task6/Task6/Solver.cs
@@ -11,5 +11,44 @@ namespace Task6
     /// <remarks>Taylor sum</remarks>
     public static class Solver
     {
+        public static float Divide(float x, float y, float epsilon)
+        {
+            if (x <= epsilon)
+            {
+                throw new ArgumentOutOfRangeException(nameof(x), "x should be positive");
+            }
+
+            if (y <= epsilon)
+            {
+                throw new ArgumentOutOfRangeException(nameof(y), "y should be positive");
+            }
+
+            float result1 = 0;
+            float result2 = x * y;
+            while (Abs(result1 - result2) > epsilon)
+            {
+                float m = (result1 + result2) * (float)0.5;
+                float c = m * y - x;
+                if (c < 0)
+                {
+                    result1 = m;
+                }
+                else
+                {
+                    result2 = m;
+                }
+            }
+            return result1;
+        }
+
+        private static float Abs(float x)
+        {
+            if (x >= 0)
+            {
+                return x;
+            }
+
+            return -x;
+        }
     }
 }
--- AkvelonMicrosoftTraining/Intensified/Task6/Task6Tests/SolverTests.cs
+++ AkvelonMicrosoftTraining/Intensified/Task6/Task6Tests/SolverTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Task6;
 
@@ -8,6 +7,68 @@ namespace Task6Tests
     [TestClass]
     public class SolverTests
     {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DivideByNegativeTest()
+        {
+            this.DivideTest(1, -10);
+        }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DivideNegativeTest()
+        {
+            this.DivideTest(-10, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void DivideByZeroTest()
+        {
+            this.DivideTest(1, 0);
+        }
+
+        [TestMethod]
+        public void DivideByTwoTest()
+        {
+            this.DivideTest(1, 2);
+        }
+
+        [TestMethod]
+        public void DivideTwoByThreeTest()
+        {
+            this.DivideTest(2, 3);
+        }
+
+        [TestMethod]
+        public void DivideByOneTest()
+        {
+            this.DivideTest(1, 1);
+        }
+
+        [TestMethod]
+        public void DivideThreeByFour()
+        {
+            this.DivideTest(3, 4);
+        }
+
+        [TestMethod]
+        public void DivideFloatByFloat()
+        {
+            this.DivideTest((float)3.5325, (float)40.53151);
+        }
+
+        private void DivideTest(float x, float y)
+        {
+            // arrange
+            const float epsilon = (float) 0.01;
+            var expected = Math.Abs(y) > epsilon ? x / y : 0;
+
+            // act
+            var result = Solver.Divide(x, y, epsilon);
+
+            // assert
+            Assert.AreEqual(expected, result, epsilon);
+        }
     }
 }
