07be030cf6a80eacb45e717d71c0c4a97fcbb86a
EUROPE\v-hepav <v-hepav@microsoft.com>
2017-09-29 20:10:39 +0300

    Task 7 of Intensified course done

--- AkvelonMicrosoftTraining/Intensified/Task7/Task7/Solver.cs
+++ AkvelonMicrosoftTraining/Intensified/Task7/Task7/Solver.cs
@@ -12,6 +12,56 @@ namespace Task7
     /// </summary>
     public static class Solver
     {
+        public static string CanBeSortedByReverse(int[] input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
 
+            var sortedAscending = Copy(input);
+            Sort(sortedAscending, ascending: true);
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] != sortedAscending[i] && input[input.Length - 1 - i] != sortedAscending[i])
+                {
+                    return "Not Possible";
+                }
+            }
+
+            return "Possible";
+        }
+
+        private static int[] Copy(int[] array)
+        {
+            var result = new int[array.Length];
+            for (var arrayIndex = 0; arrayIndex < array.Length; arrayIndex++)
+            {
+                result[arrayIndex] = array[arrayIndex];
+            }
+
+            return result;
+        }
+
+        private static void Sort(int[] array, bool ascending)
+        {
+            for (var arrayLeftIndex = 0; arrayLeftIndex < array.Length - 1; arrayLeftIndex++)
+            {
+                for (var arrayRightIndex = arrayLeftIndex + 1; arrayRightIndex < array.Length; arrayRightIndex++)
+                {
+                    if (ascending ^ array[arrayLeftIndex] < array[arrayRightIndex])
+                    {
+                        Swap(ref array[arrayLeftIndex], ref array[arrayRightIndex]);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(ref int a, ref int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
     }
 }
--- AkvelonMicrosoftTraining/Intensified/Task7/Task7Tests/SolverTests.cs
+++ AkvelonMicrosoftTraining/Intensified/Task7/Task7Tests/SolverTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Task7;
 
@@ -8,6 +7,62 @@ namespace Task7Tests
     [TestClass]
     public class SolverTests
     {
-      
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SolverTestNull()
+        {
+            SolverTest("Not Possible", null);
+        }
+
+        [TestMethod]
+        public void SolverTestEmpty()
+        {
+            SolverTest("Possible", new int[] { });
+        }
+
+        [TestMethod]
+        public void SolverTestSingle()
+        {
+            SolverTest("Possible", new int[] {1});
+        }
+
+        [TestMethod]
+        public void SolverTestSorted()
+        {
+            SolverTest("Possible", new int[] {1, 2});
+        }
+
+        [TestMethod]
+        public void SolverTestSortedDescending()
+        {
+            SolverTest("Possible", new int[] { 2, 1 });
+        }
+
+        [TestMethod]
+        public void SolverTestImpossible()
+        {
+            SolverTest("Not Possible", new int[] { 2, 1, 3});
+        }
+
+        [TestMethod]
+        public void SolverTestLongerPossible()
+        {
+            SolverTest("Possible", new int[] { 1, 2, 5, 4, 3, 6, 7 });
+        }
+
+        [TestMethod]
+        public void SolverTestLongerImpossible()
+        {
+            SolverTest("Not Possible", new int[] { 1, 3, 2, 4, 5, 6, 7 });
+        }
+
+        private void SolverTest(string expectedResult, int[] input)
+        {
+            // act
+            var result = Solver.CanBeSortedByReverse(input);
+
+            // assert
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
