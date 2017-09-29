ea76c140e2af41f0329b5da3ab70de3934a8c922
EUROPE\v-hepav <v-hepav@microsoft.com>
2017-09-29 20:52:39 +0300

    Task 8 of Intensified course done

--- AkvelonMicrosoftTraining/Intensified/Task8/Task8/Solver.cs
+++ AkvelonMicrosoftTraining/Intensified/Task8/Task8/Solver.cs
@@ -1,14 +1,71 @@
-﻿using System;
-using System.CodeDom;
-using System.Runtime.ExceptionServices;
-
-namespace Task8
+﻿namespace Task8
 {
     /// <summary>
     /// 8. Given a LinkedList like 1->2->3->4->5->6. Modify it as: 1->6->2->5->3->4.
     /// </summary>
     public static class Solver
     {
-     
+        public static void Modify(ref Node head)
+        {
+            if (head == null)
+            {
+                return;
+            }
+
+            var fast = head;
+            var slow = head;
+            while (fast != null && fast.Next != null)
+            {
+                slow = slow.Next;
+                fast = fast.Next.Next;
+            }
+
+            var head1 = head;
+            var head2 = slow.Next;
+            slow.Next = null;
+            ReverseList(ref head2);
+
+            // 4) Merge alternate nodes
+            head = new Node(0); // Assign dummy Node
+
+            // curr is the pointer to this dummy Node, which will
+            // be used to form the new list
+            Node current = head;
+            while (head1 != null || head2 != null)
+            {
+                // First add the element from list
+                if (head1 != null)
+                {
+                    current.Next = head1;
+                    current = current.Next;
+                    head1 = head1.Next;
+                }
+
+                // Then add the element from second list
+                if (head2 != null)
+                {
+                    current.Next = head2;
+                    current = current.Next;
+                    head2 = head2.Next;
+                }
+            }
+
+            head = head.Next;
+        }
+
+        private static void ReverseList(ref Node head)
+        {
+            Node prev = null;
+            Node current = head;
+            while (current != null)
+            {
+                var next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            }
+
+            head = prev;
+        }
     }
 }
--- AkvelonMicrosoftTraining/Intensified/Task8/Task8Tests/SolverTests.cs
+++ AkvelonMicrosoftTraining/Intensified/Task8/Task8Tests/SolverTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Task8;
 
@@ -8,6 +7,92 @@ namespace Task8Tests
     [TestClass]
     public class SolverTests
     {
-      
+        [TestMethod]
+        public void ModifyEmptyTest()
+        {
+            this.ModifyTest(new int[] { }, new int[] { });
+        }
+
+        [TestMethod]
+        public void ModifySingleTest()
+        {
+            this.ModifyTest(new int[] {1 }, new int[] {1 });
+        }
+
+        [TestMethod]
+        public void ModifyDoubleTest()
+        {
+            this.ModifyTest(new int[] { 1,2 }, new int[] { 1, 2 });
+        }
+
+        [TestMethod]
+        public void ModifyThreeTest()
+        {
+            this.ModifyTest(new int[] { 1, 3, 2 }, new int[] { 1, 2, 3 });
+        }
+
+        [TestMethod]
+        public void ModifySixTest()
+        {
+            this.ModifyTest(new int[] { 1, 6, 2, 5, 3, 4 }, new int[] { 1, 2, 3, 4, 5, 6 });
+        }
+
+        private void ModifyTest(int[] expectedOutput, int[] input)
+        {
+            // arrange
+            var head = ArrayToList(input);
+
+            // act
+            Solver.Modify(ref head);
+
+            // assert
+            CollectionAssert.AreEqual(expectedOutput, ListToArray(head));
+        }
+
+        private static Node ArrayToList(int[] input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            Node result = null;
+            Node current = null;
+            for (var inputIndex = 0; inputIndex < input.Length; inputIndex++)
+            {
+                var next = new Node(input[inputIndex]);
+                if (current != null)
+                {
+                    current.Next = next;
+                }
+
+                current = next;
+                if (result == null)
+                {
+                    result = current;
+                }
+            }
+
+            return result;
+        }
+
+        private static int[] ListToArray(Node head)
+        {
+            var resultCount = 0;
+            for (var current = head; current != null; current = current.Next)
+            {
+                resultCount++;
+            }
+
+            var result = new int[resultCount];
+            var resultIndex = 0;
+            for (var current = head; current != null; current = current.Next)
+            {
+                result[resultIndex] = current.Value;
+                resultIndex++;
+            }
+
+            return result;
+        }
     }
 }
