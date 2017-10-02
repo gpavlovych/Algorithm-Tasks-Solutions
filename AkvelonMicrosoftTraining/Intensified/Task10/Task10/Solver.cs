8bbf27e7172074e3a5c6c75e7ce0f62ad48e8424
EUROPE\v-hepav <v-hepav@microsoft.com>
2017-10-02 19:04:33 +0300

    Task 10 of Intensified is done

--- AkvelonMicrosoftTraining/Intensified/Task10/Task10/Solver.cs
+++ AkvelonMicrosoftTraining/Intensified/Task10/Task10/Solver.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.CodeDom;
+using System.Runtime.CompilerServices;
 using System.Runtime.ExceptionServices;
 
 namespace Task10
@@ -11,5 +12,51 @@ namespace Task10
     /// </summary>
     public static class Solver
     {
+        #region LCG
+
+        private static int seed = 0;
+
+        // the LCG parameters below are the recommended ones from literature
+        private const long multiplier = 1664525;
+        private const long increment = 1013904223;
+        private const int modulus = int.MaxValue;
+
+        /// <summary>
+        /// Initializes the random generator
+        /// </summary>
+        public static void InitSeed()
+        {
+            seed = (int)DateTime.UtcNow.Ticks;
+        }
+
+        /// <summary>
+        /// Linear congruential generator aka LCG (uniformly distributed 0 to modulus-1 values)
+        /// </summary>
+        /// <returns></returns>
+        private static int Rand()
+        {
+            seed = (int)((multiplier * seed + increment) % modulus);
+            return seed;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Returns 1 or 0 with 50% probability each
+        /// </summary>
+        /// <returns></returns>
+        private static int Rand50()
+        {
+            return Rand() & 0x1;//or Rand()%2;
+        }
+
+        /// <summary>
+        /// Returns 1 or 0 with 75% and 25% probability, respectively.
+        /// </summary>
+        /// <returns></returns>
+        public static int Rand75()
+        {
+            return Rand50() | Rand50();
+        }
     }
 }
--- AkvelonMicrosoftTraining/Intensified/Task10/Task10Tests/SolverTests.cs
+++ AkvelonMicrosoftTraining/Intensified/Task10/Task10Tests/SolverTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Task10;
 
 namespace Task10Tests
@@ -8,6 +6,40 @@ namespace Task10Tests
     [TestClass]
     public class SolverTests
     {
+        [TestInitialize]
+        public void Init()
+        {
+            Solver.InitSeed();
+        }
 
+        [TestMethod]
+        public void Verify25And75()
+        {
+            // arrange
+            var total = 10000;
+            var total0 = 0;
+            var total1 = 0;
+
+            // act
+            for (var i = 0; i < total; i++)
+            {
+                var nextRand = Solver.Rand75();
+                if (nextRand == 0)
+                {
+                    total0++;
+                }
+
+                if (nextRand == 1)
+                {
+                    total1++;
+                }
+            }
+            
+            // assert
+            var probability0 = (double) total0 / total;
+            var probability1 = (double) total1 / total;
+            Assert.AreEqual(0.25, probability0, 0.01);
+            Assert.AreEqual(0.75, probability1, 0.01);
+        }
     }
 }
