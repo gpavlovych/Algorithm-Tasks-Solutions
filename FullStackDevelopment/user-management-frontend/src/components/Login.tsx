6d063d030f935322116d7794d671f4208468033c
EUROPE\v-hepav <v-hepav@microsoft.com>
2017-10-18 14:46:49 +0200

    Irrelevant content removed from this repo (and added to another one)

--- FullStackDevelopment/user-management-frontend/src/components/Login.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-
-export class Login extends React.Component {
-    render() {
-        // tab accepting current url
-        return <label>whatever</label>;
-    }
-}
--- FullStackDevelopment/user-management-frontend/src/components/Welcome.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-export class Welcome extends React.Component {
-    render() {
-        return <label>whatever</label>;
-    }
-}
